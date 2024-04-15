8ef07c5cf8347208d2ddbfd661b950fb37f75a2f
kimeunwoo <gumman0620@gmail.com>
2024-04-15 19:43:36 +0900

    publishing(#4): 캐릭터 생성 페이지 퍼블리싱 중간단계

--- src/components/index-page/NewCharacter.tsx
+++ src/components/index-page/NewCharacter.tsx
@@ -2,7 +2,7 @@ import * as S from './style';
 
 export default function NewCharacter() {
     return (
-        <S.BackgroundColor backgroundcolor="#5F6F52">
+        <S.BackgroundColor backgroundcolor="#5F6F52" display="flex" flexdirection="column" alignitems="center">
             <S.CreateCharacterText>캐릭터를 생성하세요!</S.CreateCharacterText>
             <S.InfoNewCharacter>
                 <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/warning-icon.svg"></img>
@@ -12,7 +12,19 @@ export default function NewCharacter() {
                 상점에서 다른 캐릭터로 변경이 가능합니다. <br />
                 모쪼록 생성한 캐릭터와 행복한 시간 보내시길 바랍니다.
             </S.InfoNewCharacter>
-            <S.ChoiseCharacterBox></S.ChoiseCharacterBox>
+            <S.ChoiseCharacterBox display="flex" flexdirection="column" alignitems="center">
+                <S.InputNicknameBox> 닉 네 임</S.InputNicknameBox>
+                <S.ShowFirstCharacterBox display="flex">
+                    <S.LeftArrow>
+                        <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/ArrowLeft.png"></img>
+                    </S.LeftArrow>
+                    <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/firstCat.png"></img>
+                    <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/firstDog.png"></img>
+                    <S.RightArrow>
+                        <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/ArrowRight.png"></img>
+                    </S.RightArrow>
+                </S.ShowFirstCharacterBox>
+            </S.ChoiseCharacterBox>
         </S.BackgroundColor>
     );
 }
--- src/components/index-page/style.ts
+++ src/components/index-page/style.ts
@@ -3,22 +3,19 @@
 import styled from 'styled-components';
 
 interface BackgroundColor {
-    props?: string;
     backgroundcolor?: string;
+    display?: string;
+    flexdirection?: string;
+    alignitems?: string;
 }
 
 export const BackgroundColor = styled.body<BackgroundColor>`
     height: 100vh;
-    width: 100wh;
+    width: 100vw;
     background-color: ${(props) => props.backgroundcolor};
-`;
-
-export const InputNickname = styled.input`
-    width: 847px;
-    height: 117px;
-    border: 1px solid black;
-    border-radius: 60px;
-    background: #d7e9b9;
+    display: ${(props) => props.display};
+    flex-direction: ${(props) => props.flexdirection};
+    align-items: ${(props) => props.alignitems};
 `;
 
 interface MainLogo {
@@ -49,7 +46,10 @@ interface MainLogoTextProps {
     margintop?: number;
     textshadow?: number;
 }
-
+/**
+ 로그인 페이지 텍스트들
+ @params width : number, height : number, font-size : number, margin-top : number 
+ */
 export const MainLogoText = styled.div<MainLogoTextProps>`
     width: ${(props) => props.width}px;
     height: ${(props) => props.height}px;
@@ -62,7 +62,10 @@ interface MainPowerBtn {
     width?: number;
     height?: number;
 }
-
+/**
+ 소셜 로그인 성공시 생성되는 접속버튼
+ @params width : number, height : number
+ */
 export const MainPowerBtn = styled.div<MainPowerBtn>`
     width: ${(props) => props.width}px;
     height: ${(props) => props.height}px;
@@ -73,12 +76,20 @@ export const MainPowerBtn = styled.div<MainPowerBtn>`
     filter: drop-shadow(5px 2px 2px grey);
 `;
 
+/**
+ 캐릭터를 생성하세요 텍스트입니다. 
+ @parmas text : "none" | ""
+ @return text-decoration : none=>글자 수를 의미
+*/
 export const CreateCharacterText = styled.div`
     font-size: 64px;
     color: white;
     margin-bottom: 2%;
 `;
 
+/**
+ 캐릭터 생성 시 주의사항 안내 텍스트
+*/
 export const InfoNewCharacter = styled.div`
     width: 80%;
     height: 25%;
@@ -86,22 +97,74 @@ export const InfoNewCharacter = styled.div`
     border-radius: 33px;
     background-color: white;
     opacity: 0.8;
-    font-size: 18px;
+    font-size: 2.5vh;
     line-height: 150%;
     margin-bottom: 1.3%;
 `;
 
-export const ChoiseCharacterBox = styled.div`
+/**
+ 캐릭터 생성 최상위 부모 박스
+ */
+
+interface ChoiseCharacterBox {
+    display?: string;
+    flexdirection?: string;
+    alignitems?: string;
+}
+
+export const ChoiseCharacterBox = styled.div<ChoiseCharacterBox>`
     width: 80%;
     height: 48%;
     background-color: white;
-    opacity: 0.8;
-    text-align: center;
+    background-color: rgba(256, 256, 256, 0.8);
     border-radius: 33px;
+    display: ${(props) => props.display};
+    flex-direction: ${(props) => props.flexdirection};
+    align-items: ${(props) => props.alignitems};
 `;
-
+/**
+ 캐릭터 생성 페이지 '캐릭터를 생성하세요!'
+ */
 export const StartText = styled.div`
     font-size: 64px;
     color: white;
     margin-bottom: 2%;
+`;
+
+/**
+ 캐릭터 생성 페이지 닉네임 입력 input
+ */
+export const InputNicknameBox = styled.div`
+    margin-top: 1%;
+    width: 64%;
+    height: 17%;
+    border-radius: 33px;
+    background-color: white;
+    font-size: 6.5vh;
+    line-height: 130%;
+`;
+/**
+ 캐릭터 생성 박스
+ */
+export const ShowFirstCharacterBox = styled.div<ChoiseCharacterBox>`
+    margin-top: 2%;
+    width: 64%;
+    height: 57%;
+    border-radius: 33px;
+    background-color: white;
+    display: ${(props) => props.display};
+    flex-direction: ${(props) => props.flexdirection};
+    align-items: ${(props) => props.alignitems};
+`;
+
+export const LeftArrow = styled.div`
+    img {
+        margin-right: 10px;
+    }
+`;
+
+export const RightArrow = styled.div`
+    img {
+        margin-left: 10px;
+    }
 `;
