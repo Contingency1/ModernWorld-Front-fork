291301f5046984e1622a9d47b87ab604f6bfa8d0
chamjin <playingme@naver.com>
2024-05-13 20:53:09 +0900

    publish(#5): 선물함 기초 레이아웃 설정, 인벤토리 컴포넌트 쪼개기 (진행중)

---
+++ src/components/my-page/contents/inventory/InventoryHeader.tsx
@@ -0,0 +1,10 @@
+export default function InventoryHeader() {
+  return (
+    <>
+      <S.BookMarkHeader>
+        <S.TMenuDiv>오브제</S.TMenuDiv>
+        <S.TMenuDiv>캐릭터</S.TMenuDiv>
+      </S.BookMarkHeader>
+    </>
+  );
+}
--- src/components/my-page/contents/mail-box/MailBox.tsx
+++ src/components/my-page/contents/mail-box/MailBox.tsx
@@ -7,7 +7,37 @@ export default function MailBox() {
       <S.ContentSection>
         <S.BookMarkBody>
           <SM.Grid>
-            <SM.MailBoxSection></SM.MailBoxSection>
+            <SM.MailBoxSection>
+              <SM.MarginDiv margin="0.5vh 0 1vh 0" fontSize="28px">
+                보낸 선물 (3)
+              </SM.MarginDiv>
+              <SM.UserInfo>
+                <img
+                  src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/gift-icon.png"
+                  alt="gift"
+                  width="20vw"
+                />
+                <SM.MarginDiv margin="1vw">
+                  재진 님에게 보냈습니다.
+                </SM.MarginDiv>
+              </SM.UserInfo>
+
+              <SM.ContentsView height="25vh"></SM.ContentsView>
+              <S.PageSection>
+                <img
+                  src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/ArrowLeft.png"
+                  alt="<"
+                  width="30vw"
+                />
+                1 / 2
+                <img
+                  src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/ArrowRight.png"
+                  alt="<"
+                  width="30vw"
+                />
+              </S.PageSection>
+              <SM.ContentsView height="20vh"></SM.ContentsView>
+            </SM.MailBoxSection>
             <SM.MailBoxSection></SM.MailBoxSection>
           </SM.Grid>
           <S.BookMarkSide>
