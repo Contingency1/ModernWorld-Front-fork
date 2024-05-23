7797ed9ebea16f7658cde0ed9a9ef653dff784e9
kimeunwoo <gumman0620@gmail.com>
2024-05-23 17:16:05 +0900

    test

--- src/components/index-page/LoginPage.tsx
+++ src/components/index-page/LoginPage.tsx
@@ -15,6 +15,7 @@ export default function MainPage() {
                     img_height={'100%'}
                     // img_margin_right={'-1%'}
                     img_margin_left=""
+                    font={110}
                 >
                     <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/%EB%A1%9C%EA%B7%B8%EC%9D%B8+%EC%95%84%EC%9D%B4%EC%BD%98+%EB%A1%9C%EA%B3%A0+%EB%AA%A8%EC%9D%8C/n%EB%A1%9C%EA%B3%A0-removebg-preview.png"></img>
                     네이버 로그인
@@ -27,6 +28,7 @@ export default function MainPage() {
                     img_height={'80%'}
                     img_margin_right={'6%'}
                     img_margin_left="4%"
+                    font={110}
                 >
                     <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/%EB%A1%9C%EA%B7%B8%EC%9D%B8+%EC%95%84%EC%9D%B4%EC%BD%98+%EB%A1%9C%EA%B3%A0+%EB%AA%A8%EC%9D%8C/%EC%B9%B4%EC%B9%B4%EC%98%A4%EB%A1%9C%EA%B3%A0-removebg-preview.png"></img>
                     카카오 로그인
@@ -39,6 +41,7 @@ export default function MainPage() {
                     img_height={'50%'}
                     img_margin_right={'4%'}
                     img_margin_left="8%"
+                    font={80}
                 >
                     <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/%EB%A1%9C%EA%B7%B8%EC%9D%B8+%EC%95%84%EC%9D%B4%EC%BD%98+%EB%A1%9C%EA%B3%A0+%EB%AA%A8%EC%9D%8C/%EA%B5%AC%EA%B8%80%EB%A1%9C%EA%B3%A02-removebg-preview.png"></img>
                     Google 계정으로 로그인
