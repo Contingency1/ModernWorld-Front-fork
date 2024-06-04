d0316f9157150bf2db6b89af1682d0cbd9602f8e
kimeunwoo <gumman0620@gmail.com>
2024-06-04 20:31:59 +0900

    bug fix(#15):깃 충돌 수정...2

--- src/app/api/GetItems.tsx
+++ src/app/api/GetItems.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function GetItems() {
     try {
         const response = await axios.get('http://54.180.98.58:3000/inventory/users/1?status=true');
-        return response.data;
+        return response.data[0].item.image;
     } catch (error) {
         console.error(error);
         return null;
