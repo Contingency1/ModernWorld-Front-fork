220605da05806e49829b849f1a033fa93a2bbe55
chamjin <playingme@naver.com>
2024-06-24 17:36:44 +0900

    feat(#14) : LeftSection 데이터 불러오기

--- src/components/my-page/user/LeftSection.tsx
+++ src/components/my-page/user/LeftSection.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import * as S from "@/components/my-page/user/style";
 import CharacterInfo from "./CharacterInfo";
 import PointInfo from "./PointInfo";
 import Category from "./Category";
+import getUser from "@/app/api/getUser";
 
 export default function LeftSection() {
+  getUser(2);
+
   return (
     <>
       <S.OutLineSection width="48vh" height="82vh">
--- src/components/my-page/user/PointInfo.tsx
+++ src/components/my-page/user/PointInfo.tsx
@@ -1,14 +1,20 @@
 import * as S from "@/components/my-page/user/style";
+import { useAtom } from "jotai";
+import { userDataAtom } from "@/state/userAtoms";
 
 export default function PointInfo() {
+  const [data] = useAtom(userDataAtom);
+  const currentPoint = data.currentPoint;
+  const accumulationPoint = data.accumulationPoint;
+
   return (
     <>
       <S.PointInfo color="rgba(255, 255, 255, 0.5)">
         <S.PointTitle>사용 가능 포인트</S.PointTitle>
-        <S.PointContent>2,000 point</S.PointContent>
+        <S.PointContent>{currentPoint}</S.PointContent>
         <S.PointHr />
         <S.PointTitle>누적 포인트</S.PointTitle>
-        <S.PointContent>10,000 point</S.PointContent>
+        <S.PointContent>{accumulationPoint}</S.PointContent>
       </S.PointInfo>
     </>
   );
