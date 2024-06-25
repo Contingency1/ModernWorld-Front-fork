0d14f131d9f0386cc45dc743e6d989aa7385e852
kimeunwoo <gumman0620@gmail.com>
2024-06-25 17:45:06 +0900

    (#27): 배포 전 통합

--- src/components/my-room/MyRoom.tsx
+++ src/components/my-room/MyRoom.tsx
@@ -1,28 +1,28 @@
-import { GetItems } from '@/app/api/GetItems';
-import * as S from './style';
+// import { GetItems } from '@/app/api/GetItems';
+// import * as S from './style';
 
-export async function MyRoom() {
-    const userItemArray = await GetItems();
-    return (
-        <>
-            <S.RootDiv width="100vw" height="100vh">
-                <S.BackGroundGrid>
-                    <S.WallPaper />
-                    <S.Floor />
-                </S.BackGroundGrid>
-                <S.ItemsGrid>
-                    {userItemArray.map(
-                        (e: {
-                            item: {
-                                image: string;
-                                name: string;
-                            };
-                        }) => (
-                            <S.ItemImageSize src={e.item.image} key={e.item.name}></S.ItemImageSize>
-                        )
-                    )}
-                </S.ItemsGrid>
-            </S.RootDiv>
-        </>
-    );
-}
+// export async function MyRoom() {
+//     const userItemArray = await GetItems();
+//     return (
+//         <>
+//             <S.RootDiv width="100vw" height="100vh">
+//                 <S.BackGroundGrid>
+//                     <S.WallPaper />
+//                     <S.Floor />
+//                 </S.BackGroundGrid>
+//                 <S.ItemsGrid>
+//                     {userItemArray.map(
+//                         (e: {
+//                             item: {
+//                                 image: string;
+//                                 name: string;
+//                             };
+//                         }) => (
+//                             <S.ItemImageSize src={e.item.image} key={e.item.name}></S.ItemImageSize>
+//                         )
+//                     )}
+//                 </S.ItemsGrid>
+//             </S.RootDiv>
+//         </>
+//     );
+// }
