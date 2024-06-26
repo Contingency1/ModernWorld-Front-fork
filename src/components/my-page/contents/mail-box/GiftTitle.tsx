d53980ca8dc88170ac7736ce3dda54f94302f529
kimeunwoo <gumman0620@gmail.com>
2024-06-26 14:05:37 +0900

    (#27):배포 전 통합 및 오류 수정

--- src/components/my-page/contents/mail-box/GiftTitle.tsx
+++ src/components/my-page/contents/mail-box/GiftTitle.tsx
@@ -1,19 +1,19 @@
-import * as S from "@/components/my-page/contents/mail-box/style";
+import * as S from '@/components/my-page/contents/mail-box/style';
 
-export default function GiftTitle(props) {
-  return (
-    <>
-      <S.MarginDiv margin="0.5vh 0 1vh 0" fontSize="28px">
-        {props.title} 선물 (3)
-      </S.MarginDiv>
-      <S.UserInfo>
-        <img
-          src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/gift-icon.png"
-          alt="gift"
-          width="20vw"
-        />
-        <S.MarginDiv margin="1vw">재진 님에게 보냈습니다.</S.MarginDiv>
-      </S.UserInfo>
-    </>
-  );
+export default function GiftTitle(props: { title: string }) {
+    return (
+        <>
+            <S.MarginDiv margin="0.5vh 0 1vh 0" fontSize="28px">
+                {props.title} 선물 (3)
+            </S.MarginDiv>
+            <S.UserInfo>
+                <img
+                    src="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/gift-icon.png"
+                    alt="gift"
+                    width="20vw"
+                />
+                <S.MarginDiv margin="1vw">재진 님에게 보냈습니다.</S.MarginDiv>
+            </S.UserInfo>
+        </>
+    );
 }
