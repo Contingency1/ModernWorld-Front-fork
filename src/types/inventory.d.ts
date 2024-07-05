9d99cb9c437a1a69924bc5de1224fb1117e74acb
chamjin <playingme@naver.com>
2024-07-05 11:09:38 +0900

    setting(#49): style 선택적 타입 -> Partial 화 진행

---
+++ src/types/inventory.d.ts
@@ -0,0 +1,2 @@
+/** 인벤토리 api type */
+interface inventoryApiType {}
--- src/types/style.d.ts
+++ src/types/style.d.ts
@@ -1,31 +1,33 @@
-export interface StyleType {
-  height?: string;
-  width?: string;
-  margin?: string;
-  borderTop?: string;
-  borderBottom?: string;
-  borderRight?: string;
-  borderLeft?: string;
-  backColor?: string;
-  color?: string;
-  img?: string;
-  fontSize?: string;
-  inputcolor?: string;
-  check?: boolean;
-  display?: string;
-  flexdirection?: string;
-  alignitems?: string;
-  textcolor?: string;
-  img_width?: string;
-  img_height?: string;
-  img_margin_right?: string;
-  img_margin_left?: string;
-  font?: string;
-  margintop?: string;
-  props?: string;
+interface Props {
+  height: string;
+  width: string;
+  margin: string;
+  borderTop: string;
+  borderBottom: string;
+  borderRight: string;
+  borderLeft: string;
+  backColor: string;
+  color: string;
+  img: string;
+  fontSize: string;
+  inputcolor: string;
+  check: boolean;
+  display: string;
+  flexdirection: string;
+  alignitems: string;
+  textcolor: string;
+  img_width: string;
+  img_height: string;
+  img_margin_right: string;
+  img_margin_left: string;
+  font: string;
+  margintop: string;
+  props: string;
   //MainLogo
-  grid1?: string;
-  grid2?: string;
-  grid3?: string;
-  grid4?: string;
+  grid1: string;
+  grid2: string;
+  grid3: string;
+  grid4: string;
 }
+
+export interface StyleType extends Partial<Props> {}
