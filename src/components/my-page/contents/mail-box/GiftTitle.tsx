a0525f68f8e95cdf178b35aab348258907afe08f
chamjin <playingme@naver.com>
2024-07-16 17:00:41 +0900

    modify(#45): title 수정

--- src/components/my-page/contents/mail-box/GiftTitle.tsx
+++ src/components/my-page/contents/mail-box/GiftTitle.tsx
@@ -2,22 +2,54 @@
 
 import * as S from '@/components/my-page/contents/mail-box/style';
 import { useEffect, useState } from 'react';
-import { useAtomValue } from 'jotai';
+import { useAtomValue, useAtom } from 'jotai';
 import {
   mailBoxSelectAtom,
   senderDataAtom,
   receiverDataAtom,
   viewPageAtom,
 } from '@/states/mailboxAtoms';
+import MAILBOX from '@/app/api/mailBox';
 
 export default function GiftTitle(props: { title: string }) {
   const type = useAtomValue(mailBoxSelectAtom);
-  const senderData = useAtomValue(senderDataAtom);
-  const receiverData = useAtomValue(receiverDataAtom);
+  const [senderData, setSenderData] = useAtom(senderDataAtom);
+  const [receiverData, setReceiverData] = useAtom(receiverDataAtom);
   const page = useAtomValue(viewPageAtom);
-  const [data] = useState(
-    props.title.includes('보낸') ? senderData : receiverData,
+  const [data, setData] = useState(
+    /보낸/.test(props.title) ? senderData : receiverData,
   );
+
+  useEffect(() => {
+    if (/보낸/.test(props.title)) {
+      setData(senderData);
+    } else {
+      setData(receiverData);
+    }
+  }, [type, props.title]);
+
+  const getData = async (type: number) => {
+    try {
+      if (type === 0) {
+        const senderPosts = await MAILBOX.getPostsList('senderNo');
+        const receiverPosts = await MAILBOX.getPostsList('receiverNo');
+        setSenderData(senderPosts);
+        setReceiverData(receiverPosts);
+      } else if (type === 1) {
+        const senderPresents = await MAILBOX.getPresentsList('senderNo');
+        const receiverPresents = await MAILBOX.getPresentsList('receiverNo');
+        setSenderData(senderPresents);
+        setReceiverData(receiverPresents);
+      }
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+  };
+
+  useEffect(() => {
+    getData(type);
+  }, [type]);
+
   return (
     <>
       <S.MarginDiv margin="0.5vh 0 1vh 0" fontSize="28px">
@@ -34,7 +66,7 @@ export default function GiftTitle(props: { title: string }) {
           width="20vw"
         />
         <S.MarginDiv margin="1vw">
-          {'수정중'} 님에게{' '}
+          {'d'} 님에게{' '}
           {props.title === '보낸 선물' ? '보냈습니다.' : '받았습니다.'}
         </S.MarginDiv>
       </S.UserInfo>
--- src/components/my-page/contents/mail-box/MailBoxBody.tsx
+++ src/components/my-page/contents/mail-box/MailBoxBody.tsx
@@ -1,34 +1,12 @@
 'use client';
 
 import * as S from '@/components/my-page/contents/mail-box/style';
-import MAILBOX from '@/app/api/mailBox';
-import { useEffect } from 'react';
 import MailBoxBodyEle from './MailBoxBodyEle';
-import { useAtomValue, useSetAtom } from 'jotai';
-import {
-  mailBoxSelectAtom,
-  senderDataAtom,
-  receiverDataAtom,
-} from '@/states/mailboxAtoms';
+import { useAtomValue } from 'jotai';
+import { mailBoxSelectAtom } from '@/states/mailboxAtoms';
 
 export default function MailBoxBody() {
   const type = useAtomValue(mailBoxSelectAtom);
-  const setSenderData = useSetAtom(senderDataAtom);
-  const setReceiverData = useSetAtom(receiverDataAtom);
-
-  const getData = async (mailBoxType: number) => {
-    if (mailBoxType) {
-      setSenderData(await MAILBOX.getPostsList('senderNo'));
-      setReceiverData(await MAILBOX.getPostsList('receiverNo'));
-    } else {
-      setSenderData(await MAILBOX.getPresentsList('senderNo'));
-      setReceiverData(await MAILBOX.getPresentsList('receiverNo'));
-    }
-  };
-
-  useEffect(() => {
-    getData(type);
-  }, [type]);
 
   return (
     <>
