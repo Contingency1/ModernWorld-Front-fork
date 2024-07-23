7aef92df78c3115064ec6d17681d6ba0498cf89e
chamjin <playingme@naver.com>
2024-07-23 16:48:39 +0900

    feat(#83):상점 아이템 클릭시 모달창 띄우기

---
+++ src/components/shop-page/ItemClick.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import SHOP from '@/app/api/shop';
+import * as S from '@/components/my-page/contents/inventory/style';
+import { isModalOpenAtom, selectItemTypeAtom } from '@/states/shopAtoms';
+import { ShopDataType } from '@/types/shop';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { useEffect, useState } from 'react';
+
+export default function ItemClick(props: { no: number }) {
+  const [itemInfo, setItemInfo] = useState<ShopDataType | null>(null);
+  const [characterInfo, setCharacterInfo] = useState<ShopDataType | null>(null);
+  const selectItemType = useAtomValue(selectItemTypeAtom);
+  const setModalClose = useSetAtom(isModalOpenAtom);
+
+  const handleModalClose = () => {
+    setModalClose(false);
+  };
+
+  const getItemInfo = async (no: number) => {
+    const response = await SHOP.getSelectItem(no);
+    setItemInfo(response);
+  };
+
+  const getCharacterInfo = async (no: number) => {
+    const response = await SHOP.getSelectCharacter(no);
+    setCharacterInfo(response);
+  };
+
+  useEffect(() => {
+    if (selectItemType === 0) {
+      getItemInfo(props.no);
+    } else {
+      getCharacterInfo(props.no);
+    }
+  }, [selectItemType]); // selectItemType을 의존성 배열에 추가
+
+  return (
+    <>
+      <S.ModalBody>
+        <S.ModalItemDiv>
+          <S.Img
+            img={
+              selectItemType === 0
+                ? itemInfo?.image ?? ''
+                : characterInfo?.image ?? ''
+            }
+          />
+        </S.ModalItemDiv>
+        <S.ModalFont>
+          {selectItemType === 0
+            ? itemInfo?.name ?? ''
+            : characterInfo?.name ?? ''}
+        </S.ModalFont>
+        <S.ModalFont>
+          {selectItemType === 0
+            ? itemInfo?.description ?? ''
+            : characterInfo?.description ?? ''}
+        </S.ModalFont>
+        <S.ModalButtonSection>
+          <S.ModalInButton>선물하기</S.ModalInButton>
+          <S.ModalInButton>구매하기</S.ModalInButton>
+        </S.ModalButtonSection>
+        <S.ModalExit
+          img="https://wang0514.s3.ap-northeast-2.amazonaws.com/page/exit.png"
+          onClick={handleModalClose}
+        />
+      </S.ModalBody>
+    </>
+  );
+}
--- src/states/shopAtoms.ts
+++ src/states/shopAtoms.ts
@@ -5,3 +5,4 @@ export const themeTypeAtom = atom<
   '봄 테마' | '여름 테마' | '가을 테마' | '겨울 테마'
 >('봄 테마');
 export const charactersTypeAtom = atom<'cat' | 'dog'>('dog');
+export const isModalOpenAtom = atom(false);
