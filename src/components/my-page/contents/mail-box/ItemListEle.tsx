22095c55d5b5c7e1908ff405f24a693cda3bda9b
chamjin <playingme@naver.com>
2024-08-20 14:45:15 +0900

    feat(#143):펫 방 만들기 + 오류 해결

--- src/components/my-page/contents/mail-box/ItemListEle.tsx
+++ src/components/my-page/contents/mail-box/ItemListEle.tsx
@@ -11,15 +11,16 @@ export default function ItemListEle(props: any) {
   const [receiverName, setReceiverName] = useState('');
   const [status, setStatus] = useState('');
 
+  console.log(props);
   useEffect(() => {
     if (props.data) {
       if (type === 0) {
-        setSenderName(props.data.userPostReceiverNo.nickname);
-        setReceiverName(props.data.userPostSenderNo.nickname);
+        setSenderName(props.data.userPostReceiverNo?.nickname);
+        setReceiverName(props.data.userPostSenderNo?.nickname);
         setStatus(props.data.check ? '읽음' : '안읽음');
       } else {
-        setSenderName(props.data.userPresentReceiverNo.nickname);
-        setReceiverName(props.data.userPresentSenderNo.nickname);
+        setSenderName(props.data.userPresentReceiverNo?.nickname);
+        setReceiverName(props.data.userPresentSenderNo?.nickname);
         getPresentStatus(props.data.status);
       }
     }
---
+++ src/components/my-page/contents/my-pet-room/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MyPetRoomBody = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #e9eff1;
+  border-radius: 20px;
+`;
+
+export const RoomBorder = styled.div`
+  width: 90%;
+  height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 45px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+`;
