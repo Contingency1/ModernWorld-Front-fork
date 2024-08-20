69aab7f69d19cb001326d846c79bc8920f33c1ea
kimeunwoo <gumman0620@gmail.com>
2024-08-20 16:04:52 +0900

    publish(#8):강고 마을 페이지 수정

--- src/components/village-page/Village.tsx
+++ src/components/village-page/Village.tsx
@@ -7,9 +7,14 @@ import Link from 'next/link';
 
 export default function Village(props: { animal: string }) {
   return (
-    <S.BackgroundDisplay color="#CACACA">
+    <S.BackgroundDisplay color={props.animal === 'cat' ? '#CACACA' : '#E5D9B6'}>
       <S.Title>
-        <img src="https://wang0514.s3.ap-northeast-2.amazonaws.com/items/KakaoTalk_20240509_151108128.png"></img>
+        <img
+          src={
+            props.animal === 'cat'
+              ? 'https://wang0514.s3.ap-northeast-2.amazonaws.com/items/KakaoTalk_20240509_151108128.png'
+              : 'https://wang0514.s3.ap-northeast-2.amazonaws.com/items/%EA%B0%95%EC%95%84%EC%A7%80+%EB%A7%88%EC%9D%84.png'
+          }></img>
       </S.Title>
       <Link href="/my-page">
         <S.ExitButton
@@ -17,8 +22,11 @@ export default function Village(props: { animal: string }) {
             'https://wang0514.s3.ap-northeast-2.amazonaws.com/page/exit.png'
           }></S.ExitButton>
       </Link>
-      <S.TheBiggestBox color="#ffffff">
-        <SearchBox inputColor="#ffffff"></SearchBox>
+      <S.TheBiggestBox color={props.animal === 'cat' ? '#ffffff' : '#FFFBEE'}>
+        <SearchBox
+          inputColor={
+            props.animal === 'cat' ? '#ffffff' : '#FFFBEE'
+          }></SearchBox>
         <S.FlexBox>
           <Pagenation animal={props.animal}></Pagenation>
         </S.FlexBox>
