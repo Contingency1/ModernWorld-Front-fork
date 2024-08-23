332d05e10182acc98f8a7eee34c24ee077cf9de4
kimeunwoo <gumman0620@gmail.com>
2024-08-23 16:39:23 +0900

    bugfix(#167):컴포넌트 함수명 앞글자 대문자로 변경

--- src/app/previewVillageUsers/[userId]/comment/page.tsx
+++ src/app/previewVillageUsers/[userId]/comment/page.tsx
@@ -6,7 +6,7 @@ import Reply from '@/components/village-page/comment/reply/Reply';
 import { useAtomValue } from 'jotai';
 import { ModalStateAtom } from '@/states/reply';
 
-export default function commentPage({
+export default function CommentPage({
   params,
 }: {
   params: { userId: number };
