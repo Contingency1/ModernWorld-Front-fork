89288143d2bc66dc9e23278d7ed02eea32e2eb5a
kimeunwoo <gumman0620@gmail.com>
2024-08-28 10:47:24 +0900

    refactor(#172):조영은

--- src/utils/rockScissorsPaper.ts
+++ src/utils/rockScissorsPaper.ts
@@ -1,6 +1,6 @@
 export const RockSicssorsPaperArray = ['Scissors', 'Rock', 'Paper'];
 export const RockSicssorsPaperImgArray = [
-  'https://wang0514.s3.ap-northeast-2.amazonaws.com/items/%EA%B0%80%EC%9C%84%EB%B0%94%EC%9C%84%EB%B3%B4/%EA%B0%80%EC%9C%84.svg',
+  'https://ma7-production-s3.s3.ap-northeast-2.amazonaws.com/page/%EA%B0%80%EC%9C%84%EB%B0%94%EC%9C%84%EB%B3%B4/%EA%B0%80%EC%9C%84.svg',
   'https://wang0514.s3.ap-northeast-2.amazonaws.com/items/%EA%B0%80%EC%9C%84%EB%B0%94%EC%9C%84%EB%B3%B4/%EB%B0%94%EC%9C%84.svg',
   'https://wang0514.s3.ap-northeast-2.amazonaws.com/items/%EA%B0%80%EC%9C%84%EB%B0%94%EC%9C%84%EB%B3%B4/%EB%B3%B4.svg',
 ];
