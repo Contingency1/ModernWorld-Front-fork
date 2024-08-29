d9c5b5b96690f2bd3d0d236b902340a746b98631
chamjin <playingme@naver.com>
2024-08-29 16:02:23 +0900

    bugfix(#170):오류 수정

--- src/utils/serviceInfo.ts
+++ src/utils/serviceInfo.ts
@@ -4,11 +4,11 @@ export const serviceDescriptions: DescriptionType[] = [
   {
     id: 1,
     title: '닉네임',
-    description: '- 닉네임은 바꿀 수 없습니다.',
+    description: ['- 닉네임은 바꿀 수 없습니다.'],
   },
   {
     id: 2,
     title: '회원탈퇴',
-    description: '- 30일 이내에 재가입시 기존 아이디가 복구됩니다.',
+    description: ['- 30일 이내에 재가입시 기존 아이디가 복구됩니다.'],
   },
 ];
