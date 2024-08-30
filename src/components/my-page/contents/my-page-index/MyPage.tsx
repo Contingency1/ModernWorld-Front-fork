9564045cfddf564d36f62a65984d748c8c2923e6
chamjin <playingme@naver.com>
2024-08-30 13:39:46 +0900

    modify(#170): 소셜네임 사용

--- src/components/my-page/contents/my-page-index/MyPage.tsx
+++ src/components/my-page/contents/my-page-index/MyPage.tsx
@@ -110,7 +110,9 @@ export default function MyPageIndex() {
               이　　름
             </S.UserInfoContent>
             <S.UserInfoContent width="30vw" $backColor="#FFDEDE">
-              {indexUserInfo.nickname ? indexUserInfo.nickname : '이름 없음'}
+              {indexUserInfo.nickname ??
+                indexUserInfo.socialName ??
+                '이름 없음'}
               <S.Font
                 $fontSize="16px"
                 color="#FF3131"
