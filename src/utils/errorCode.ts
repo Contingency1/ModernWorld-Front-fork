1fcdb68d2ee21c4209b39498fca10566099e834d
chamjin <playingme@naver.com>
2024-09-04 20:26:08 +0900

    modify(#170):상점 에러핸들링 수정

--- src/utils/errorCode.ts
+++ src/utils/errorCode.ts
@@ -1,4 +1,7 @@
 export const SHOP_ERROR_MESSAGES: Record<string, string> = {
   "User doesn't have enough point.": '포인트가 충분하지 않아요!',
   'User already has this character': '이미 보유 중인 캐릭터예요!',
+  'User already owns the item.': '이미 보유 중인 아이템이에요!',
+  "Couldn't find receiver.": '받는 이를 찾을 수 없어요!',
+  "Item doesn't exist.": '해당 아이템이 존재하지 않아요!',
 };
